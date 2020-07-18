7e4194c13a1985bd534e7e342ac18bca67038e01
Welyab Paula <welyab.exe@gmail.com>
2020-07-18 17:18:10 -0300

    initial commit

---
+++ src/main/java/com/github/senfb/cadastronfe/CadastroNfeApplication.java
@@ -0,0 +1,13 @@
+package com.github.senfb.cadastronfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroNfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroNfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/senfb/cadastronfe/CadastroNfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.senfb.cadastronfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
